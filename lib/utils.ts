ff6c6e55479f0af544c3a92c923e6832d1319dde
hajiox <ts@aizu-tv.com>
2025-06-15 20:17:18 +0900

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,21 @@
-// lib/utils.ts
-
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// ★この関数を追加
+// 数値をカンマ区切りにする関数
 export const nf = (num: number | null | undefined): string => {
   if (num === null || num === undefined) {
     return '0';
   }
   return num.toLocaleString();
+};
+
+// ★ 見つからなかった 'formatDateJST' 関数をここに追加
+export const formatDateJST = (date: Date): string => {
+  return format(date, 'yyyy-MM-dd HH:mm:ss', { locale: ja });
 };
