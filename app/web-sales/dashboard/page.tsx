77ba20fca624bd8243a00a561e8e1c84d7680d39
hajiox <ts@aizu-tv.com>
2025-06-18 14:26:42 +0900

    Update page.tsx

--- app/web-sales/dashboard/page.tsx
+++ app/web-sales/dashboard/page.tsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 import WebSalesSummaryCards from "@/components/websales-summary-cards"
 import WebSalesRankingTable from "@/components/websales-ranking-table"
 import WebSalesEditableTable from "@/components/web-sales-editable-table"
-import WebSalesCharts from "@/components/websales-charts"
 
 export const dynamic = 'force-dynamic'
 
@@ -32,8 +31,6 @@ export default function WebSalesDashboardPage() {
       </header>
 
       <WebSalesSummaryCards month={month} refreshTrigger={refreshTrigger} />
-      
-      <WebSalesCharts month={month} refreshTrigger={refreshTrigger} />
       
       <WebSalesEditableTable month={month} onDataSaved={handleDataSaved} />
       
