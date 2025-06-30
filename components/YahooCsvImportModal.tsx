14d57432c01f392f2f1b8eb1b779bd9d82bbff5d
hajiox <ts@aizu-tv.com>
2025-06-30 09:24:56 +0900

    Update YahooCsvImportModal.tsx

--- components/YahooCsvImportModal.tsx
+++ components/YahooCsvImportModal.tsx
@@ -97,7 +97,7 @@ export default function YahooCsvImportModal({
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ csvContent }),
+        body: JSON.stringify({ csvContent: csvContent }),
       });
 
       const result = await response.json();
