547161cc9465308de3e330d8d37103f2cf348e89
hajiox <ts@aizu-tv.com>
2025-07-20 13:00:32 +0900

    Update db.ts

--- types/db.ts
+++ types/db.ts
@@ -1,1 +1,17 @@
-
+// /types/db.ts
+export interface WebSalesData {
+  product_id: string
+  product_name: string
+  price: number
+  profit_rate?: number
+  amazon_count: number
+  rakuten_count: number
+  yahoo_count: number
+  mercari_count: number
+  base_count: number
+  qoo10_count: number
+  total_count: number
+  series?: string
+  series_code?: number
+  product_code?: number
+}
