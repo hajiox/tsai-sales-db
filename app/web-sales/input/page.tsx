6cdf4dd65f7df5a5ea522ba5591b3c3970222e98
hajiox <ts@aizu-tv.com>
2025-08-21 09:39:37 +0900

    Make web-sales pages client only

--- app/web-sales/input/page.tsx
+++ app/web-sales/input/page.tsx
@@ -1,4 +1,8 @@
-"use client"
+// /app/web-sales/input/page.tsx
+'use client'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 import Sidebar from "@/components/sidebar"
 import WebSalesInputView from "@/components/web-sales-input-view"
 export default function WebSalesInputPage() {
